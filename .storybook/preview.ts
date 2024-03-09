87fe9c1a90198413f20b7416b57899437d9e35ef
peschee <63866+peschee@users.noreply.github.com>
2024-03-09 19:44:37 +0100

    Initial commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/web-components';
+import { setCustomElementsManifest } from '@storybook/web-components';
+import * as cem from './custom-elements.json';
+
+setCustomElementsManifest(cem);
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
